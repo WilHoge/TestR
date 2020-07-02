051f1b143944579da45d5b12edeb90213f9a6d87
Wilfried Hoge <hoge@de.ibm.com>
2020-07-02 10:27:39 +0000

    R model

---
+++ assets/rstudio/save-model.R
@@ -0,0 +1,22 @@
+install.packages("pmml")
+install.packages("nnet")
+
+library(nnet)
+library(pmml)
+
+ird <- data.frame(rbind(iris3[,,1], iris3[,,2], iris3[,,3]),
+                  species = factor(c(rep("s",50), rep("c", 50), rep("v", 50))))
+samp <- c(sample(1:50,25), sample(51:100,25), sample(101:150,25))
+ir.nn2 <- nnet(species ~ ., data = ird, subset = samp, size = 2, rang = 0.1,
+               decay = 5e-4, maxit = 200)
+
+pmmlmodel <- pmml(ir.nn2)
+saveXML(pmmlmodel,file = "IrisNet.xml")
+
+pmmlmodel <- pmml(myModel, model.name = "Churn_Logistic_Regression_Model", 
+                  app.name = "Churn_LogReg", 
+                  description = "Modelo de Regresion para Demo", 
+                  copyright = NULL, transforms = NULL, unknownValue = NULL, 
+                  weights = NULL)
+
+saveXML(pmmlmodel,file = "IrisNet.xml")
